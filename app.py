230ca17fc36e21f6dc15d71d3516261bed93977a
Alter-up <135228265+Alter-up@users.noreply.github.com>
2024-02-17 19:08:59 +0400

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,9 @@ GUILD_ID = 1208095549778043000 #The ID of the guild you want them to join
 ROLE_IDS = [0] #List of the IDs of the roles you want them to get
 AUTORISATION_URL = "" #The obtained URL
 
-
+@app.route('/')
+def hello_world():
+    return 'Hello, world!'
 
 
 @app.route('/')
