74b59a12f658a042696f504f9a98475f33e01213
Alter-up <135228265+Alter-up@users.noreply.github.com>
2024-02-18 19:27:15 +0400

    Update app.py

--- app.py
+++ app.py
@@ -48,15 +48,8 @@ def logout():
     session.pop("access_token")
     return redirect("/")
 
-@app.route("/oauth/callback")
-def oauth_callback():
-    code = request.args["code"]
-    access_token = client.oauth.get_access_token(
-        code, redirect_uri=REDIRECT_URI
-    ).access_token
-    session["access_token"] = access_token
 
-    return redirect("/")
+  
 
 @app.route('/callback')
 def callback():
@@ -116,8 +109,16 @@ def callback():
     #Put the request
     response = requests.put(url=url, headers=headers, json=data)
 
-    print(response.text)
-    return redirect(REDIRECT_URL)
+   # print(response.text)
+  #  return redirect(REDIRECT_URL)
+
+  code = request.args["code"]
+    access_token = client.oauth.get_access_token(
+        code, redirect_uri=REDIRECT_URI
+    ).access_token
+    session["access_token"] = access_token
+
+    return redirect("/")
 
 if __name__ == "__main__":
       app.run(debug=True)
