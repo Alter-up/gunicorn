57e069c2682ee57a6077769fdc2e10bbed57a27a
Alter-up <135228265+Alter-up@users.noreply.github.com>
2024-02-18 18:38:24 +0400

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,9 @@ from zenora import APIClient
 BOT_TOKEN = "MTIwODA5NTQwMTA5NDQxNDM4Nw.GHZQxY.w378-X2fZztsDafTxHREhH947I4rOCZd8-q2ss"
 CLIENT_SECRET = "d4rJ2-ql9Zp92-GbdainnyPRrzdwhr6y"
 CLIENT_ID = 1208095401094414387  # Enter your bot's client ID
-REDIRECT_URI = (
-    f"https://tough-lingerie-bear.cyclic.app/callback"  # Your Oauth redirect URI
-)
-OAUTH_URL = f"https://discord.com/api/oauth2/authorize?client_id={CLIENT_ID}&redirect_uri={quote(REDIRECT_URI)}&response_type=code&scope=identify"
+REDIRECT_URI = "https://tough-lingerie-bear.cyclic.app/callback"  # Your Oauth redirect URI
+
+
 
 app = Flask(__name__)
 client = APIClient(BOT_TOKEN, client_secret=CLIENT_SECRET)
