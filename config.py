6b19425e91dcbb7c5f704c46d130e092782f4fcd
Alter-up <135228265+Alter-up@users.noreply.github.com>
2024-02-18 18:26:31 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,30 +1,10 @@
-# Copyright (c) 2022 DevGuyAhnaf
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 from urllib.parse import quote
 
 PORT = 5000
-BOT_TOKEN = "put your bot token here"
-CLIENT_SECRET = "put your bot's client secret here"
-CLIENT_ID = 123456789  # Enter your bot's client ID
+BOT_TOKEN = "MTIwODA5NTQwMTA5NDQxNDM4Nw.GHZQxY.w378-X2fZztsDafTxHREhH947I4rOCZd8-q2ss"
+CLIENT_SECRET = "d4rJ2-ql9Zp92-GbdainnyPRrzdwhr6y"
+CLIENT_ID = 1208095401094414387  # Enter your bot's client ID
 REDIRECT_URI = (
-    f"http://localhost:{PORT}/oauth/callback"  # Your Oauth redirect URI
+    f"https://tough-lingerie-bear.cyclic.app/callback"  # Your Oauth redirect URI
 )
 OAUTH_URL = f"https://discord.com/api/oauth2/authorize?client_id={CLIENT_ID}&redirect_uri={quote(REDIRECT_URI)}&response_type=code&scope=identify"
