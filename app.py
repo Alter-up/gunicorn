ff84bc633cf5dbe6dc750ede9f708f5ebde49cd0
Alter-up <135228265+Alter-up@users.noreply.github.com>
2024-02-18 13:28:41 +0400

    Update app.py

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ def callback():
     print(response.text)
     return redirect(REDIRECT_URL)
 
-@app.route('/')
+@app.route('/,  methods=["GET"]')
 def main():
     return render_template(
         'index.html',
