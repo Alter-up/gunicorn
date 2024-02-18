456c7c2ec63a72154ed0b2bebd20067eb2f4534c
Alter-up <135228265+Alter-up@users.noreply.github.com>
2024-02-18 20:25:58 +0400

    Update app.py

--- app.py
+++ app.py
@@ -54,13 +54,7 @@ def logout():
 
 @app.route('/callback')
 def callback():
-   code = request.args["code"]
-    access_token = client.oauth.get_access_token(
-        code, redirect_uri=REDIRECT_URI
-    ).access_token
-    session["access_token"] = access_token
-
-    return redirect("/")
+   
 
     print("flag")
     if request.values.get('error'):
@@ -121,6 +115,13 @@ def callback():
     print(response.text)
     return redirect(OAUTH2_REDIRECT_URI)
 
+    code = request.args["code"]
+    access_token = client.oauth.get_access_token(
+        code, redirect_uri=REDIRECT_URI
+    ).access_token
+    session["access_token"] = access_token
+
+    return redirect("/")
  
 
 if __name__ == "__main__":
