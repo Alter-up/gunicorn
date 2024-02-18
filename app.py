f11d99cb527638fc9cba9743a6e5f64817b3defd
Alter-up <135228265+Alter-up@users.noreply.github.com>
2024-02-18 20:51:19 +0400

    Update app.py

--- app.py
+++ app.py
@@ -63,66 +63,7 @@ def index():
 
 
  
-@app.route('/callback')
-def callback():
-    print("flag")
-    if request.values.get('error'):
-        return request.values['error']
 
-    args = request.args
-    code = args.get('code')
-
-    data = {
-        'client_id': OAUTH2_CLIENT_ID,
-        'client_secret': OAUTH2_CLIENT_SECRET,
-        'grant_type': 'authorization_code',
-        'code': code,
-        'redirect_uri': OAUTH2_REDIRECT_URI
-    }
-    headers = {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
-
-    r = requests.post("https://discord.com/api/v10/oauth2/token", data=data, headers=headers)
-    r.raise_for_status()
-
-    #Get the acces token
-    access_token = r.json()["access_token"]
-
-    #Get info of the user, to get the id
-    url = f"{API_ENDPOINT}/users/@me"
-
-    headers = {
-        "Authorization": f"Bearer {access_token}",
-        'Content-Type': 'application/json'
-    }
-
-    #This will contain the information
-    response = requests.get(url=url, headers=headers)
-
-    print(response.json())
-
-    #Extract the id
-    user_id = response.json()["id"]
-
-    #URL for adding a user to a guild
-    url = f"{API_ENDPOINT}/guilds/{GUILD_ID}/members/{user_id}"
-
-    headers = {
-        "Authorization": f"Bot {BOT_TOKEN}"
-    }
-
-    #These lines specifies the data given. Acces_token is mandatory, roles is an array of role ids the user will start with.
-    data = {
-        "access_token": access_token,
-        "roles": ROLE_IDS
-    }
-
-    #Put the request
-    response = requests.put(url=url, headers=headers, json=data)
-
-    print(response.text)
-    return redirect(REDIRECT_URL)
 
 @app.route('/callback')
 def callback():
