63bebc9dab0d620511fd78fb8c7d2ab097a110d3
Alter-up <135228265+Alter-up@users.noreply.github.com>
2024-02-18 18:27:08 +0400

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import sys
-from .config import run
-from gunicorn.app.wsgiapp import PORT
+from .config import PORT
+from gunicorn.app.wsgiapp import run
 if __name__ == '__main__':
     sys.argv = "gunicorn --bind 0.0.0.0:5000 app:app".split()
     sys.exit(run())
