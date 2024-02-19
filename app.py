7cfb9d180b96e7279313ef4f0a4a6b0d39de7df3
Alter-up <135228265+Alter-up@users.noreply.github.com>
2024-02-19 14:05:07 +0400

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ BOT_TOKEN = "MTIwODA5NTQwMTA5NDQxNDM4Nw.GHZQxY.w378-X2fZztsDafTxHREhH947I4rOCZd8
 REDIRECT_URL = "https://tough-lingerie-bear.cyclic.app/me"  # Your Oauth redirect URI
 GUILD_ID = 1208721793532039209 #The ID of the guild you want them to join
 ROLE_IDS = [0] #List of the IDs of the roles you want them to get
-AUTORISATION_URL = "me.html" #The obtained URL
+AUTORISATION_URL = "claim.html" #The obtained URL
 
 
 API_BASE_URL = 'https://discord.com/api/v10'
