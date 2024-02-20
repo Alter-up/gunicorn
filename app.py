927b19416397e23740e060b6d734a7f578657dd9
Alter-up <135228265+Alter-up@users.noreply.github.com>
2024-02-20 09:37:54 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,106 +1,55 @@
-from flask import Flask, request, redirect, render_template
-import requests
-from routes.discord_oauth import DiscordOauth
+import sys, subprocess
+# from replit import clear
 
+subprocess.check_call([sys.executable, "-m", "pip", "install", 'discord-oauth2.py'])
+# clear()
 
-API_ENDPOINT = 'https://discord.com/api/v10'
-TOKEN_URL = "https://discord.com/api/oauth2/token"
+from flask import Flask, redirect, request
+import discordoauth2
+import os, json
 
-# https://analogone.pages.dev
-OAUTH2_CLIENT_ID = "1208095401094414387" #Your client ID
-OAUTH2_CLIENT_SECRET = "d4rJ2-ql9Zp92-GbdainnyPRrzdwhr6y" #Your client secret
-OAUTH2_REDIRECT_URI = "https://plain-leg-warmers-crab.cyclic.app/callback" #Your redirect URL
-BOT_TOKEN = "MTIwODA5NTQwMTA5NDQxNDM4Nw.GHZQxY.w378-X2fZztsDafTxHREhH947I4rOCZd8-q2ss" #"Your application token here"
-REDIRECT_URL = "https://plain-leg-warmers-crab.cyclic.app/me"  # Your Oauth redirect URI
-GUILD_ID = 1208721793532039209 #The ID of the guild you want them to join
-ROLE_IDS = [0] #List of the IDs of the roles you want them to get
-AUTORISATION_URL = "" #The obtained URL
-
-
-app = Flask(__name__)
+app = Flask('Discord OAuth2 Example')
+client = discordoauth2.Client(id=1208095401094414387, secret=d4rJ2-ql9Zp92-GbdainnyPRrzdwhr6y,
+redirect="https://plain-leg-warmers-crab.cyclic.app/oauth2", bot_token=MTIwODA5NTQwMTA5NDQxNDM4Nw.GHZQxY.w378-X2fZztsDafTxHREhH947I4rOCZd8-q2ss
 
 @app.route('/')
 def main():
-    return redirect(AUTORISATION_URL)
+  return redirect("https://discord.com/oauth2/authorize?client_id=1208095401094414387&response_type=code&redirect_uri=https%3A%2F%2Fplain-leg-warmers-crab.cyclic.app%2Fcallback&scope=identify+guilds+guilds.join%20guilds.members.read")
 
+@app.route('/oauth2', methods=["GET"])
+def oauth():
+    tokenObject = client.exchange_code(request.args.get('code'))
+    print("refresh token: "+tokenObject.refresh_token)
+    
+    identify = tokenObject.fetch_identify()
+    connections = tokenObject.fetch_connections()
+    guilds = tokenObject.fetch_guilds()
+    return f"""<pre style=\"word-wrap: break-word; white-space: pre-wrap;\">
+{json.dumps(identify, indent=2)}
+</pre>
+<br>
+<pre style=\"word-wrap: break-word; white-space: pre-wrap;\">
+{json.dumps(connections, indent=2)}
+</pre>
+<br>
+<pre style=\"word-wrap: break-word; white-space: pre-wrap;\">
+{json.dumps(guilds, indent=2)}
+</pre>
+<br>
+test joining servers!
+<form action="/oauth2/guild_join" method="post">
+  <input name="nick" placeholder="put a join nickname here" type="text">
+  <input name="access" type="hidden" value=" {tokenObject.token}">
+  <input type="submit">
+</form>
+"""
 
-@app.route('/callback, methods=['GET']')
-def callback():
-    print("flag")
-    if request.values.get('error'):
-        return request.values['error']
+@app.route('/oauth2/guild_join', methods=["POST"])
+def oauth_guild_join():
+  access = client.from_access_token(request.form["access"])
 
-    args = request.args
-    code = args.get('code')
-
-    data = {
-        'client_id': OAUTH2_CLIENT_ID,
-        'client_secret': OAUTH2_CLIENT_SECRET,
-        'grant_type': 'authorization_code',
-        'code': code,
-        'redirect_uri': OAUTH2_REDIRECT_URI
-    }
-    headers = {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
-
-    r = requests.post("https://discord.com/api/v10/oauth2/token", data=data, headers=headers)
-    r.raise_for_status()
-
-    #Get the acces token
-    access_token = r.json()["access_token"]
-
-    #Get info of the user, to get the id
-    url = f"{API_ENDPOINT}/users/@me"
-
-    headers = {
-        "Authorization": f"Bearer {access_token}",
-        'Content-Type': 'application/json'
-    }
-
-    #This will contain the information
-    response = requests.get(url=url, headers=headers)
-
-    print(response.json())
-
-    #Extract the id
-    user_id = response.json()["id"]
-
-    #URL for adding a user to a guild
-    url = f"{API_ENDPOINT}/guilds/{GUILD_ID}/members/{user_id}"
-
-    headers = {
-        "Authorization": f"Bot {BOT_TOKEN}"
-    }
-
-    #These lines specifies the data given. Acces_token is mandatory, roles is an array of role ids the user will start with.
-    data = {
-        "access_token": access_token,
-        "roles": ROLE_IDS
-    }
-
-    #Put the request
-    response = requests.put(url=url, headers=headers, json=data)
-
-    print(response.text)
-    return redirect(DiscordOauth.login_url)
-
-
-# Route for dashboard
-@app.route('/dashboard', methods=['GET'])
-def dashboard():
-    code = request.args.get('code')
-    access_token = DiscordOauth.get_access_token(code)
-
-    user_object = DiscordOauth.get_user(access_token)
-    user_guild_object = DiscordOauth.get_user_current_guild(access_token)
-
-    id, avatar, username, usertag = user_object.get('id'), user_object.get('avatar'), user_object.get('username'), \
-                                    user_object.get('discriminator')
-
-    return render_template('dashboard.html', render_user_avatar=f'https://cdn.discordapp.com/avatars/{id}/{avatar}.png',
-                           render_username=f'{username}#{usertag}', render_guild=user_guild_object)
-
+  access.join_guild(1043078102357651486, nick=request.form["nick"])
+  return "", 204
 
 if __name__ == '__main__':
     app.run(debug=True)
