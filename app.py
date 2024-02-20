d1d1488fe2c2f18477546633c862eceb9c913fec
Alter-up <135228265+Alter-up@users.noreply.github.com>
2024-02-20 09:07:57 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, g, render_template, request, redirect, session, url_for, jsonify
-from requests_oauthlib import OAuth2Session
-import requests
+from quart import Quart, render_template, request, session, redirect, url_for
+from quart_discord import DiscordOAuth2Session, requires_authorization, Unauthorized
+from discord.ext import ipc
+import os
 
 
 API_ENDPOINT = 'https://discord.com/api/v10'
@@ -16,61 +17,63 @@ GUILD_ID = 1208721793532039209 #The ID of the guild you want them to join
 ROLE_IDS = [0] #List of the IDs of the roles you want them to get
 AUTORISATION_URL = "" #The obtained URL
 
-API_BASE_URL = 'https://discord.com/api/v10'
-AUTHORIZATION_BASE_URL = API_BASE_URL + '/oauth2/authorize'
-TOKEN_URL = API_BASE_URL + '/oauth2/token'
+app.config["DISCORD_CLIENT_ID"] = 1208095401094414387  # Discord client ID.
+app.config[
+    "DISCORD_CLIENT_SECRET"
+] = "d4rJ2-ql9Zp92-GbdainnyPRrzdwhr6y"  # Discord client secret.
+app.config[
+    "DISCORD_REDIRECT_URI"
+] = "https://plain-leg-warmers-crab.cyclic.app/callback"  # URL to your callback endpoint.
 
-app = Flask(__name__)
+app = Quart(__name__)
+ipc_client = ipc.Client(
+    secret_key="this_is_token"
+) 
 
-@app.route('/callback')
-def callback():
-    print("flag")
-    if request.values.get('error'):
-        return request.values['error']
-
-    args = request.args
-    code = args.get('code')
-
-    data = {
-        'client_id': OAUTH2_CLIENT_ID,
-        'client_secret': OAUTH2_CLIENT_SECRET,
-        'grant_type': 'authorization_code',
-        'code': code,
-        'redirect_uri': OAUTH2_REDIRECT_URI
-    }
-    headers = {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
-
-    r = requests.post("https://discord.com/api/v10/oauth2/token", data=data, headers=headers)
-    r.raise_for_status()
-
-    #Get the acces token
-    access_token = r.json()["access_token"]
-
-    #Get info of the user, to get the id
-    url = f"{API_ENDPOINT}/users/@me"
-
-    headers = {
-        "Authorization": f"Bearer {access_token}",
-        'Content-Type': 'application/json'
-    }
-
-    response = requests.get(url=url, headers=headers)
-    print(response.json())
-
-    user_id = response.json()["id"]
-    url = f"{API_ENDPOINT}/guilds/{GUILD_ID}/members/{user_id}"
-    headers = {
-        "Authorization": f"Bot {BOT_TOKEN}"
-    }
-    response = requests.put(url=url, headers=headers, json=data)
-   
-    print(response.text)
-    return response.json()['id']
+discord = DiscordOAuth2Session(app)
 
 
+@app.route("/")
+async def home():
+    return await render_template("index.html", authorized=await discord.authorized)
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route("/login")
+async def login():
+    return await discord.create_session()
+
+
+@app.route("/callback")
+async def callback():
+    try:
+        await discord.callback()
+    except Exception:
+        pass
+
+    return redirect(url_for("dashboard"))
+
+
+@app.route("/dashboard")
+async def dashboard():
+    guild_count = await ipc_client.request("get_guild_count")
+    guild_ids = await ipc_client.request("get_guild_ids")
+    user = await discord.fetch_user()
+
+    try:
+        user_guilds = await discord.fetch_guilds()
+    except:
+        return redirect(url_for("login"))
+
+    same_guilds = []
+
+    for guild in user_guilds:
+        if guild.id in guild_ids:
+            same_guilds.append(guild)
+
+    return await render_template(
+        "dashboard.html", guild_count=guild_count, matching=same_guilds, user=user
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
