87b209fdd6bd9db5bacc9ac622ec2772addc08a2
Alter-up <135228265+Alter-up@users.noreply.github.com>
2024-02-20 08:31:38 +0400

    Update app.py

--- app.py
+++ app.py
@@ -22,34 +22,6 @@ TOKEN_URL = API_BASE_URL + '/oauth2/token'
 
 app = Flask(__name__)
 
-def token_updater(token):
-    session['oauth2_token'] = token
-
-
-def make_session(token=None, state=None, scope=None):
-    return OAuth2Session(
-        client_id=OAUTH2_CLIENT_ID,
-        token=token,
-        state=state,
-        scope=scope,
-        redirect_uri=OAUTH2_REDIRECT_URI,
-        auto_refresh_kwargs={
-            'client_id': OAUTH2_CLIENT_ID,
-            'client_secret': OAUTH2_CLIENT_SECRET,
-        },
-        auto_refresh_url=TOKEN_URL,
-        token_updater=token_updater)
-
-@app.route('/')
-def index():
-    scope = request.args.get(
-        'scope',
-        'identify email connections guilds guilds.join')
-    discord = make_session(scope=scope.split(' '))
-    authorization_url, state = discord.authorization_url(AUTHORIZATION_BASE_URL)
-    session['oauth2_state'] = state
-    return redirect(authorization_url)
-
 @app.route('/callback')
 def callback():
     print("flag")
@@ -111,12 +83,6 @@ def callback():
 
 
     print(response.text)
-    discord = make_session(state=session.get('oauth2_state'))
-    token = discord.fetch_token(
-        TOKEN_URL,
-        client_secret=OAUTH2_CLIENT_SECRET,
-        authorization_response=request.url)
-    session['oauth2_token'] = token
     return redirect(url_for('.me'))
 
 
