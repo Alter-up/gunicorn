ae5bd638a6ce71d4c3bab82abfe3e38adc8abf95
Alter-up <135228265+Alter-up@users.noreply.github.com>
2024-02-20 09:54:22 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,139 +1,31 @@
-import os
-
-from flask import Flask, redirect, url_for
-from flask_discord import DiscordOAuth2Session, requires_authorization
-
+from flask import Flask, request, redirect, render_template
+from routes.discord_oauth import DiscordOauth
 
 app = Flask(__name__)
 
-app.secret_key = b"%\xe0'\x01\xdeH\x8e\x85m|\xb3\xffCN\xc9g"
-os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "true"    # !! Only in development environment.
 
-app.config["DISCORD_CLIENT_ID"] = 1208095401094414387
-app.config["DISCORD_CLIENT_SECRET"] = "d4rJ2-ql9Zp92-GbdainnyPRrzdwhr6y"
-app.config["DISCORD_BOT_TOKEN"] = "MTIwODA5NTQwMTA5NDQxNDM4Nw.GHZQxY.w378-X2fZztsDafTxHREhH947I4rOCZd8-q2ss"
-app.config["DISCORD_REDIRECT_URI"] = "https://plain-leg-warmers-crab.cyclic.app/callback"
-
-discord = DiscordOAuth2Session(app)
-
-
-HYPERLINK = '<a href="{}">{}</a>'
-
-
-def welcome_user(user):
-    dm_channel = discord.bot_request("/users/@me/channels", "POST", json={"recipient_id": user.id})
-    return discord.bot_request(
-        f"/channels/{dm_channel['id']}/messages", "POST", json={"content": "Thanks for authorizing the app!"}
-    )
-
-
-@app.route("/")
-def index():
-    if not discord.authorized:
-        return f"""
-        {HYPERLINK.format(url_for(".login"), "Login")} <br />
-        {HYPERLINK.format(url_for(".login_with_data"), "Login with custom data")} <br />
-        {HYPERLINK.format(url_for(".invite_bot"), "Invite Bot with permissions 8")} <br />
-        {HYPERLINK.format(url_for(".invite_oauth"), "Authorize with oauth and bot invite")}
-        """
-
-    return f"""
-    {HYPERLINK.format(url_for(".me"), "@ME")}<br />
-    {HYPERLINK.format(url_for(".logout"), "Logout")}<br />
-    {HYPERLINK.format(url_for(".user_guilds"), "My Servers")}<br />
-    {HYPERLINK.format(url_for(".add_to_guild", guild_id=475549041741135881), "Add me to 475549041741135881.")}    
-    """
-
-
-@app.route("/login/")
+# Route for index page
+# Provides user login capabilities
+@app.route('/login', methods=['GET'])
 def login():
-    return discord.create_session()
+    return redirect(DiscordOauth.login_url)
 
 
-@app.route("/login-data/")
-def login_with_data():
-    return discord.create_session(data=dict(redirect="/me/", coupon="15off", number=15, zero=0, status=False))
+# Route for dashboard
+@app.route('/dashboard', methods=['GET'])
+def dashboard():
+    code = request.args.get('code')
+    access_token = DiscordOauth.get_access_token(code)
+
+    user_object = DiscordOauth.get_user(access_token)
+    user_guild_object = DiscordOauth.get_user_current_guild(access_token)
+
+    id, avatar, username, usertag = user_object.get('id'), user_object.get('avatar'), user_object.get('username'), \
+                                    user_object.get('discriminator')
+
+    return render_template('dashboard.html', render_user_avatar=f'https://cdn.discordapp.com/avatars/{id}/{avatar}.png',
+                           render_username=f'{username}#{usertag}', render_guild=user_guild_object)
 
 
-@app.route("/invite-bot/")
-def invite_bot():
-    return discord.create_session(scope=["bot"], permissions=8, guild_id=464488012328468480, disable_guild_select=True)
-
-
-@app.route("/invite-oauth/")
-def invite_oauth():
-    return discord.create_session(scope=["bot", "identify"], permissions=8)
-
-
-@app.route("/callback/")
-def callback():
-    data = discord.callback()
-    redirect_to = data.get("redirect", "/")
-
-    user = discord.fetch_user()
-    welcome_user(user)
-
-    return redirect(redirect_to)
-
-
-@app.route("/me/")
-def me():
-    user = discord.fetch_user()
-    return f"""
-<html>
-<head>
-<title>{user.name}</title>
-</head>
-<body><img src='{user.avatar_url or user.default_avatar_url}' />
-<p>Is avatar animated: {str(user.is_avatar_animated)}</p>
-<a href={url_for("my_connections")}>Connections</a>
-<br />
-</body>
-</html>
-
-"""
-
-
-@app.route("/me/guilds/")
-def user_guilds():
-    guilds = discord.fetch_guilds()
-    return "<br />".join([f"[ADMIN] {g.name}" if g.permissions.administrator else g.name for g in guilds])
-
-
-@app.route("/add_to/<int:guild_id>/")
-def add_to_guild(guild_id):
-    user = discord.fetch_user()
-    return user.add_to_guild(guild_id)
-
-
-@app.route("/me/connections/")
-def my_connections():
-    user = discord.fetch_user()
-    connections = discord.fetch_connections()
-    return f"""
-<html>
-<head>
-<title>{user.name}</title>
-</head>
-<body>
-{str([f"{connection.name} - {connection.type}" for connection in connections])}
-</body>
-</html>
-
-"""
-
-
-@app.route("/logout/")
-def logout():
-    discord.revoke()
-    return redirect(url_for(".index"))
-
-
-@app.route("/secret/")
-@requires_authorization
-def secret():
-    return os.urandom(16)
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
