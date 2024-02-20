a4be9f35e98de54e4d15419916a41fdfa4d49938
Alter-up <135228265+Alter-up@users.noreply.github.com>
2024-02-20 11:15:20 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask, request, redirect, render_template
 from routes.discord_oauth import DiscordOauth
 
+OAUTH2_CLIENT_ID = "1208095401094414387" #Your client ID
+OAUTH2_CLIENT_SECRET = "d4rJ2-ql9Zp92-GbdainnyPRrzdwhr6y" #Your client secret
+OAUTH2_REDIRECT_URI = "https://plain-leg-warmers-crab.cyclic.app/callback" #Your redirect URL
+BOT_TOKEN = "MTIwODA5NTQwMTA5NDQxNDM4Nw.GHZQxY.w378-X2fZztsDafTxHREhH947I4rOCZd8-q2ss" #"Your application token here"
+REDIRECT_URL = "https://plain-leg-warmers-crab.cyclic.app/me"  # Your Oauth redirect URI
+GUILD_ID = 1208721793532039209 #The ID of the guild you want them to join
+ROLE_IDS = [0] #List of the IDs of the roles you want them to get
+AUTORISATION_URL = "" #The obtained URL
+
+
 app = Flask(__name__)
 
 
@@ -28,6 +38,60 @@ def dashboard():
     return render_template('dashboard.html', render_user_avatar=f'https://cdn.discordapp.com/avatars/{id}/{avatar}.png',
                            render_username=f'{username}#{usertag}', render_guild=user_guild_object)
 
+
+@app.route('/dashboard/callback')
+def callback():
+    print("flag")
+    if request.values.get('error'):
+        return request.values['error']
+
+    args = request.args
+    code = args.get('code')
+
+    data = {
+        'client_id': OAUTH2_CLIENT_ID,
+        'client_secret': OAUTH2_CLIENT_SECRET,
+        'grant_type': 'authorization_code',
+        'code': code,
+        'redirect_uri': OAUTH2_REDIRECT_URI
+    }
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+
+    r = requests.post("https://discord.com/api/v10/oauth2/token", data=data, headers=headers)
+    r.raise_for_status()
+
+    #Get the acces token
+    access_token = r.json()["access_token"]
+
+    #Get info of the user, to get the id
+    url = f"{API_ENDPOINT}/users/@me"
+
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        'Content-Type': 'application/json'
+    }
+
+    #This will contain the information
+    response = requests.get(url=url, headers=headers)
+
+    print(response.json())
+
+    #Extract the id
+    user_id = response.json()["id"]
+
+    #URL for adding a user to a guild
+    url = f"{API_ENDPOINT}/guilds/{GUILD_ID}/members/{user_id}"
+
+    headers = {
+        "Authorization": f"Bot {BOT_TOKEN}"
+    }
+
+    response = requests.put(url=url, headers=headers, json=data)
+
+    print(response.text)
+    return redirect(REDIRECT_URL)
 
 if __name__ == '__main__':
     app.run(debug=True)
